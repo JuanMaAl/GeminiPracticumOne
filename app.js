79f668accb9056cdfd28d61773d2c6f3b4c5ddbe
JuanMaAl <juanmaal1986@gmail.com>
2025-02-20 19:39:01 +0100

    el get tareas ordena primero por id y luego por fechas, las fechas mas proximas quedan primero y luego las fechas null ordenadas por id

--- app.js
+++ app.js
@@ -2,16 +2,35 @@ const express = require('express');
 const app = express();
 const db = require('./database');
 const port = 3000;
+const getTareasQuery = `SELECT * FROM tareas
+					 WHERE completado = 0
+					 ORDER BY id ASC` 
+
+const postTareasQuery = `INSERT INTO tareas (
+				titulo, 
+				descripcion, 
+				fecha_limite, 
+				completado
+				)
+				VALUES (?, ?, ?, ?)`;
+
 
 app.use(express.json());
 
 // GET /api/tareas
 app.get('/api/tareas', (req, res)=>{
-	db.all("SELECT * FROM tareas", (err, rows) => {
+	db.all(getTareasQuery, (err, rows) => {
 		if(err){
 			res.status(500).json({ error: err.message });
 			return;
 		}
+		// Ordenar tareas por fecha_limite
+		rows.sort((a, b)=>{
+			if(!a.fecha_limite) return 1;
+			if(!b.fecha_limite) return -1;
+			return new Date(a.fecha_limite - b.fecha_limite);
+		});
+		
 		res.json(rows);
 	});
 });
@@ -25,25 +44,19 @@ app.post('/api/tareas', (req, res)=>{
 		return res.status(400).json({error: "el título es obligatorio" });
 	}
 
-	const query = `INSERT INTO tareas (titulo, descripcion, fecha_limite, completado)
-	VALUES (?, ?, ?, ?)`;
-
-	db.run(query, [titulo, descripcion, fecha_limite, completado ? 1: 0], function(err) {
-	if (err) {
-		return res.status(500).json({ error: err.message });
-	}
-	res.status(201).json({
-		id: this.lastID,
-		titulo,
-		descripcion,
-		fecha_limite,
-		completado: !!completado
+	db.run(postTareasQuery, [titulo, descripcion, 
+	fecha_limite, completado ? 1: 0], function(err) {
+		if (err) {
+			return res.status(500).json({ error: err.message });
+		}
+		res.status(201).json({
+			id: this.lastID,
+			titulo,
+			descripcion,
+			fecha_limite,
+			completado: !!completado
 		});
 	});
-});
-
-app.get('/', (req, res)=>{
-	res.send("Hola mundo!");
 });
 
 app.listen(port, ()=>{
