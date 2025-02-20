6f942984d14b2a6c2eb1f1588110e6b4e5f2b292
JuanMaAl <juanmaal1986@gmail.com>
2025-02-20 16:51:23 +0100

    endpoint api/tareas get y post

--- app.js
+++ app.js
@@ -1,6 +1,46 @@
 const express = require('express');
 const app = express();
+const db = require('./database');
 const port = 3000;
+
+app.use(express.json());
+
+// GET /api/tareas
+app.get('/api/tareas', (req, res)=>{
+	db.all("SELECT * FROM tareas", (err, rows) => {
+		if(err){
+			res.status(500).json({ error: err.message });
+			return;
+		}
+		res.json(rows);
+	});
+});
+
+// POT /api/tareas
+app.post('/api/tareas', (req, res)=>{
+	const { titulo, descripcion, fecha_limite, completado  } = req.body;
+	
+	// Validación básica
+	if (!titulo) {
+		return res.status(400).json({error: "el título es obligatorio" });
+	}
+
+	const query = `INSERT INTO tareas (titulo, descripcion, fecha_limite, completado)
+	VALUES (?, ?, ?, ?)`;
+
+	db.run(query, [titulo, descripcion, fecha_limite, completado ? 1: 0], function(err) {
+	if (err) {
+		return res.status(500).json({ error: err.message });
+	}
+	res.status(201).json({
+		id: this.lastID,
+		titulo,
+		descripcion,
+		fecha_limite,
+		completado: !!completado
+		});
+	});
+});
 
 app.get('/', (req, res)=>{
 	res.send("Hola mundo!");
--- database.js
+++ database.js
@@ -2,11 +2,12 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 
 db.serialize(()=>{
-	db.run(`CREATE TABLE IF NO EXISTS tareas (
+	db.run(`CREATE TABLE IF NOT EXISTS tareas (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	titulo TEXT NOT NULL,
 	descripcion TEXT,
-	completado INTEGER DEFAULT 0,
+	fecha_limite TEXT,
+	completado INTEGER DEFAULT 0
 	)`);
 });
 
