86685e3b79c998417c6e4eb7b578ee7488788c53
JuanMaAl <juanmaal1986@gmail.com>
2025-02-25 19:46:16 +0100

    Tabla users en SQLite

--- database.js
+++ database.js
@@ -9,6 +9,12 @@ db.serialize(()=>{
 	fecha_limite TEXT,
 	completado INTEGER DEFAULT 0
 	)`);
+	db.run(`CREATE TABLE IF NOT EXISTS users (
+	id INTENGER PRIMARY KEY AUTOINCREMENT,
+	usuario TEXT UNIQUE,
+	password TEXT,
+	email TEXT UNIQUE
+	)`);
 });
 
 module.exports= db;
