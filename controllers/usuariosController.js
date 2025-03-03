331ff5b622eccc579d71e47237428176f9917e80
JuanMaAl <juanmaal1986@gmail.com>
2025-03-03 09:01:54 +0100

    using the hashingPassword for the creation of the user in the register

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -4,14 +4,15 @@ const bcrypt = require('bcrypt');
 // POST /api/registro
 const postUsuarios = (req, res) => {
 	const { usuario, password, email } = req.body;
+	let hashedPassword;
 	try {
-		const hashedPassword = bcrypt.hashSync(password, 10); 
+	    hashedPassword = bcrypt.hashSync(password, 10); 
 	} catch(error) {
 		console.error("Error al hashear el password: ", error);
 	}
 	
 	db.run(`INSERT INTO users (usuario, password, email) 
-	VALUES (?, ?, ?)`, [usuario, password, email],
+	VALUES (?, ?, ?)`, [usuario, hashedPassword, email],
 		function (err) {
 			if (err) {
 					return res.status(400).json({ error: err.message });
